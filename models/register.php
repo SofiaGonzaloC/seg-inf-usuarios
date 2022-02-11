802be93c68ba384f693d927d089d7c2792eeb2b2
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2022-02-10 18:39:26 -0700

    fix register

--- models/register.php
+++ models/register.php
@@ -2,48 +2,57 @@
 
 $message = '';
 
+// 
+
 // Si los campos NO estan vacios ...
-if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['password']) && !empty($_POST['password_conf'])) {
+if (!empty($_POST['username']) && !empty($_POST['nombre']) && !empty($_POST['apellido_pat']) && !empty($_POST['apellido_mat']) && !empty($_POST['telefono']) && !empty($_POST['direccion']) && !empty($_POST['password']) && !empty($_POST['password_conf'])) {
+  if($_POST['password'] == $_POST['password_conf']){
 
-  // Revisa que el email no este ya en el DB
-  $email_check = $conn->prepare("SELECT COUNT(id) FROM usuarios WHERE email=:email");
-  $email_check->bindParam(':email', $_POST['email']);
-  $email_check->execute();
+    // Revisa que el email no este ya en el DB
+    $email_check = $conn->prepare("SELECT COUNT(id) FROM usuarios WHERE username=:username");
+    $email_check->bindParam(':username', $_POST['username']);
+    $email_check->execute();
 
-  $res = $email_check->fetchColumn();
+    $res = $email_check->fetchColumn();
 
-  if ($res == 0) { // Si el email no existe en la base de datos se procede con el registro
-    // Sacar información del usuario para el proceso
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $package = $_POST['package'];
+    if ($res == 0) { // Si el email no existe en la base de datos se procede con el registro
+      // Sacar información del usuario para el proceso
+      $username     = $_POST['username'];
+      $nombre       = $_POST['nombre'];
+      $apellido_pat = $_POST['apellido_pat'];
+      $apellido_mat = $_POST['apellido_mat'];
+      $direccion    = $_POST['direccion'];
+      $telefono     = $_POST['telefono'];
 
-    // Agrega valores de nuevo user a la base de datos
-    $sql = "INSERT INTO usuarios (nombre, email, telefono, password) VALUES (:name, :email, :phone, :password);"; // retorna el id para crear la tarjeta
-    $stmt = $conn->prepare($sql);
+      // Agrega valores de nuevo user a la base de datos
+      $sql = "INSERT INTO `usuarios` (`id`, `username`, `nombre`, `apellido-paterno`, `apellido-materno`, `password`, `telefono`, `direccion`)
+              VALUES (NULL, :username, :nombre, :apellido_pat, :apellido_mat, :password, :telefono, :direccion);";
+      $stmt = $conn->prepare($sql);
 
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':phone', $phone);
+      $stmt->bindParam(':username', $username);
+      $stmt->bindParam(':nombre', $nombre);
+      $stmt->bindParam(':apellido_pat', $apellido_pat);
+      $stmt->bindParam(':apellido_mat', $apellido_mat);
+      $stmt->bindParam(':direccion', $direccion);
+      $stmt->bindParam(':telefono', $telefono);
 
-    //cifra la contraseña
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+      //cifra la contraseña
+      $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+      $stmt->bindParam(':password', $password);
 
-    $stmt->bindParam(':password', $password);
+      if ($stmt->execute()) { // Se realiza el registro y se crea la tarjeta
+        // La cuenta es creada con éxito y se manda al login
+        $message = 'Cuenta creada con éxito.';
+        header("location: ./login.php?message=$message");
+      } else {
+        $message = 'Hubo un error, intentalo de nuevo.';
+      }
 
-    if ($stmt->execute()) { // Se realiza el registro y se crea la tarjeta
-      $id = $conn->lastInsertId();
-
-      // La cuenta es creada con éxito y se manda al login
-      $message = 'Cuenta creada con éxito.';
-      header("location: ../../index.php?message=$message");
-    } else {
-      $message = 'Hubo un error, intentalo de nuevo.';
-      header("location: ./register.php?message=$message");
+    } else if ($res == 1) { // Si el email ya esta en uso
+      $message = 'El nombre de usuario ya esta en uso.';
     }
-  } else if ($res == 1) { // Si el email ya esta en uso
-    $message = 'El correo ya esta en uso, intentalo de nuevo.';
-    header("location: ./register.php?message=$message");
+
+  } else {
+    $message = 'Las contraseñas no coinciden';
   }
 }
--- views/register.php
+++ views/register.php
@@ -17,7 +17,6 @@ include_once("../models/register.php");
     <link rel="stylesheet" href="../../assets/css/demo_1/style.css">
     <link rel="shortcut icon" href="../../../assets/images/favicon.ico" />
     <link rel="stylesheet" href="../../assets/css/register-transition.css">
-
 </head>
 
 <body>
@@ -31,33 +30,38 @@ include_once("../models/register.php");
                         <div class='row'>
                             <div class='col-lg-7 col-md-8 col-sm-9 col-12 mx-auto form-wrapper'>
 
-                                <?php
-                                if ($_GET) {
-                                    echo "<p style='text-align: center;'>" . $_GET['message'] . "</p> <br>";
-                                } else {
-                                    echo "<p id='result' style='text-align: center;'></p> <br>";
-                                }
-                                ?>
+                                <?php if (!empty($message)) : ?>
+                                    <p> <?= $message ?></p>
+                                <?php endif; ?>
 
                                 <form method='POST' id='payment-form'>
                                     <div class='form-group input-rounded'>
-                                        <input type='text' class='form-control' placeholder='Nombre' name='name' />
+                                        <input type='text' class='form-control' placeholder='Username' name='username' required/>
                                     </div>
                                     <div class='form-group input-rounded'>
-                                        <input type='email' id='email' class='form-control' placeholder='Correo' name='email' />
+                                        <input type='text' class='form-control' placeholder='Nombre' name='nombre' required/>
                                     </div>
                                     <div class='form-group input-rounded'>
-                                        <input type='tel' id='phone' class='form-control' placeholder='Teléfono' name='phone' />
+                                        <input type='text' class='form-control' placeholder='Apellido Paterno' name='apellido_pat' required/>
                                     </div>
                                     <div class='form-group input-rounded'>
-                                        <input type='password' class='form-control' placeholder='Contraseña' name='password' />
+                                        <input type='text' class='form-control' placeholder='Apellido materno' name='apellido_mat' required/>
                                     </div>
                                     <div class='form-group input-rounded'>
-                                        <input type='password' class='form-control' placeholder='Confirma contraseña' name='password_conf' />
+                                        <input type='tel' id='phone' class='form-control' placeholder='Teléfono' name='telefono' required/>
+                                    </div>
+                                    <div class='form-group input-rounded'>
+                                        <input type='text' id='phone' class='form-control' placeholder='Dirección' name='direccion' required/>
+                                    </div>
+                                    <div class='form-group input-rounded'>
+                                        <input type='password' class='form-control' placeholder='Contraseña' name='password' required/>
+                                    </div>
+                                    <div class='form-group input-rounded'>
+                                        <input type='password' class='form-control' placeholder='Confirma contraseña' name='password_conf' required/>
                                     </div>
 
                                     <button class='btn btn-dark btn-block' id='next-register'>
-                                        Siguiente
+                                        Registrarse
                                     </button>
                                 </form>
 
