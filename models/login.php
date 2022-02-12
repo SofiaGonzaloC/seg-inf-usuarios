8558601a0d89b6f263f4a75d3256748ccc3626c3
LuisGonzalez558 <99559439+LuisGonzalez558@users.noreply.github.com>
2022-02-12 13:03:53 -0700

    Update login.php

--- models/login.php
+++ models/login.php
@@ -5,9 +5,9 @@ session_start(); // Inicializa la variable _SESSION
 require '../models/database.php';
 
 // Verifica que ambos campos NO esten vacios
-if (!empty($_POST['email']) && !empty($_POST['password'])) {
-  $records = $conn->prepare('SELECT id, email, password FROM users WHERE email= :email'); // Ejecuta una consulta sql. Toma id, email y pw de la tabla users
-  $records->bindParam(':email', $_POST['email']);
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
+  $records = $conn->prepare('SELECT id, username, password FROM usuarios WHERE username= :username'); // Ejecuta una consulta sql. Toma id, email y pw de la tabla users
+  $records->bindParam(':username', $_POST['username']);
   $records->execute();
   $results = $records->fetch(PDO::FETCH_ASSOC);
 
